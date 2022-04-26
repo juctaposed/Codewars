96b89955f529964835b42ea83645c111edea2bae
Justin <juc81@pitt.edu>
2022-04-26 14:23:28 -0400

    added codewar name

--- js/solution054.js
+++ js/solution054.js
@@ -1,3 +1,6 @@
+// Convert a string to an array
+// Write a function to split a string and convert it into an array of words.
+
 function stringToArray(string){
 
   const splitStr = string.split(' ')
