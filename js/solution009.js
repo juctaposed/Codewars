810ae50013316065fa575d2e66bf074282c7682a
Justin <juc81@pitt.edu>
2022-05-08 22:32:20 -0400

    completed Remove String Spaces codewar

---
+++ js/solution009.js
@@ -0,0 +1,13 @@
+//Remove String Spaces
+// Instructions - Simple, remove the spaces from the string, then return the resultant string.
+
+//Replace
+function noSpace(x){
+  return x.replace(/\s/g, "");
+}
+
+
+//Split/Join
+// function noSpace(x){
+//     return x.split(' ').join('')
+// }
