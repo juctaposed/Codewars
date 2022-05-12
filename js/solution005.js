b1d05f172e05821716682dbd7a63c564d15e349d
Justin <juc81@pitt.edu>
2022-05-11 23:36:00 -0400

    completed You Can't Code Under Pressure #1 codewar

---
+++ js/solution005.js
@@ -0,0 +1,6 @@
+// You Can't Code Under Pressure #1
+// Code as fast as you can! You need to double the integer and return it.
+function doubleInteger(i) {
+  i = i*2// i will be an integer. Double it and return it.
+  return i;
+}
