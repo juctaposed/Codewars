c4fc3ea8e4f32ac4967ed033a7ab125587322117
Justin <juc81@pitt.edu>
2022-05-15 14:57:47 -0400

    completed Convert boolean values to strings 'Yes' or 'No' codewar

---
+++ js/solution007.js
@@ -0,0 +1,18 @@
+//Convert boolean values to strings 'Yes' or 'No'.
+
+//Simple conditional
+
+function boolToWord( bool ){
+    if (bool) {
+      return 'Yes';
+    } else {
+      return 'No';
+    }
+  }
+
+
+//Ternary operator
+
+// function boolToWord( bool ){
+//     return bool ? 'Yes':'No';
+//   }
