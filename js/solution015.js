d947f5bb9c2ee73a316d5517fa701ee39c4dd1f4
Justin <juc81@pitt.edu>
2022-05-22 23:51:40 -0400

    completed Convert a Number to a String! codewar

---
+++ js/solution015.js
@@ -0,0 +1,7 @@
+//Convert a Number to a String!
+
+// Instructions - We need a function that can transform a number into a string. What ways of achieving this do you know?
+
+function numberToString(num) {
+    return num.toString()
+}
