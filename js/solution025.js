703baea2ede5fb021d5f45ef1a2dd8b8125c7354
Justin <juc81@pitt.edu>
2022-07-21 22:26:07 -0400

    reuploaded Count the Divisors of a Number solution

---
+++ js/solution025.js
@@ -0,0 +1,21 @@
+// Count the divisors of a number
+
+// Instructions - Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000.
+
+function getDivisorsCnt(n){
+    for(var d = 0, i = n; i > 0; i--){
+      if(n % i == 0) d++
+
+    }
+    return d
+}
+
+// P: our parameters will be a single whole number (integer) that will always be positive up to 500,000
+// R: we want to return all whole numbers that can go into a number
+// E: 4 --> 3 (1, 2, 4)
+//   5 --> 2 (1, 5)
+//   12 --> 6 (1, 2, 3, 4, 6, 12)
+//   30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+// P: take in a number within a function, check in a for loop up to 500,000 IF divisible 
+//  by i and a whole number, return a count of those numbers 
