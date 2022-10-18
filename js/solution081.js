63ef16a12405dd3cc31001ab26ef87c2b6d236e7
Justin <juc81@pitt.edu>
2022-10-18 02:21:59 -0400

    completed Find Nearest square number codewar

---
+++ js/solution081.js
@@ -0,0 +1,16 @@
+// <!-- Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+// Check my other katas:
+
+// Alphabetically ordered
+
+// Case-sensitive!
+
+// Not prime numbers
+//  -->
+
+function nearestSq(n){
+    return Math.round(n**0.5)**2
+}
