c5a6e6d081d2eecec4d147d7dd73546832e0d086
Justin <juc81@pitt.edu>
2023-03-16 21:37:53 -0400

    binary search algo

---
+++ js/binarySearch.js
@@ -0,0 +1,15 @@
+
+function binarySearch(nums, target) {
+    let left = 0, right = nums.length - 1
+    while (left < right) {
+        let mid = left + Math.floor((right - left + 1)/2)
+        if (target < nums[mid]) {
+            right = mid - 1
+        } else {
+            left = mid
+        }
+    }
+    return nums[left] === target ? left : -1
+};
+
+console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9), "4")
